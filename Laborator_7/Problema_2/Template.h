ce0dc1f7cc071a295d1bad94beb7d2da404c62b3
andreitablan <76064833+andreitablan@users.noreply.github.com>
2021-04-01 15:26:17 +0300

    Update Template.h

--- Laborator_7/Problema_2/Template.h
+++ Laborator_7/Problema_2/Template.h
@@ -9,25 +9,37 @@ T Callback2Equal(T a, T b)
 {
 	return a == b;
 }
+
 template<class T>
 class Vector{
 	T* vector;
 	T numar_elemente;
 public:
-	Vector(T *vector, T numar_elemente)	
+	Vector( T numar_elemente)	
 	{   
-		T* vector;
-		vector=(int*) (malloc(numar_elemente *sizeof(int)));
-		this->numar_elemente = numar_elemente;
+		this->numar_elemente = 0;
+		vector = (T*)(malloc)(this->numar_elemente * sizeof(T));
+			
 	}
+
 	void push(T x)
 	{
-		this->vector[this->numar_elemente] = x;
-		this->numar_elemente++;
+		if (this->numar_elemente != 0)
+		{
+			this->vector = (T*)(realloc(this->vector, (this->numar_elemente + 1) * sizeof(T)));
+			this->vector[this->numar_elemente] = x;
+			this->numar_elemente++;
+		}
+		else
+		{
+			this->vector=(T*) (malloc(1 *sizeof(T)));
+			this->vector[this->numar_elemente] = x;
+			this->numar_elemente++;
+		}
 	}
 	int pop()
 	{
-		return this->vector[this->numar_elemente];
+		return this->vector[this->numar_elemente-1];
 	}
 	void remove(T y)
 	{
@@ -36,6 +48,7 @@ public:
 	}
 	void insert(T y, T z)
 	{
+		this->vector = (T*)(realloc(this->vector, (this->numar_elemente + 1) * sizeof(T)));
 		this->numar_elemente++;
 		for (int i = this->numar_elemente; i > y; i++)
 			this->vector[i] = this->vector[i - 1];
@@ -47,14 +60,14 @@ public:
 			for(int j=i+1;j<this->numar_elemente;j++)
 				if (callback != nullptr)
 				{
-					if (callback(this->vector[i], this(vector[j])))
+					if (callback(this->vector[i], this->vector[j]))
 					{
 						int aux = this->vector[i];
 						this->vector[i] = this->vector[j];
 						this->vector[j] = aux;
 					}
 				}
-				else if (this->vector[i]> this(vector[j]))
+				else if (this->vector[i]> this->vector[j])
 				{
 					int aux = this->vector[i];
 					this->vector[i] = this->vector[j];
@@ -70,7 +83,7 @@ public:
 	void set(T x, T index)
 	{ 
 		if (index > this->numar_elemente)
-			printf("Eroare indexul %d este prea mare", index);
+			printf("Eroare indexul %d este prea mare\n", index);
 		this->vector[index] = x;
 	}
 	int count()
@@ -86,5 +99,11 @@ public:
 			else 
 				   if (this->vector[i] == x) return i;
 		return -1;
+	}
+	void print()
+	{
+		printf("Am apelat scrierea vectorului cu succes\n");
+		for (int i = 0; i < this->numar_elemente; i++)
+			printf("%d ", this->vector[i]);
 	}
 };
