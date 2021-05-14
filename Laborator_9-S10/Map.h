6ac7e5b4f0a00fe207db613dbe9b21a98530a3ea
andreitablan <76064833+andreitablan@users.noreply.github.com>
2021-05-14 21:04:39 +0300

    Update Map.h

--- Laborator_9-S10/Map.h
+++ Laborator_9-S10/Map.h
@@ -14,4 +14,69 @@ class Map {
 	
 	int size;	
 
+	vector = new Element[1];
+	size = 1;
+	index = 0;
+	
+	public:
+	Map()
+	{
+		vector = new Element[1];
+		size = 1;
+		index = 0;
+	}
+	
+	V& operator [] (K key)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			if (key == vector[i].key)
+				return vector[i].value;
+		}
+		vector[this->index].index = this->index;
+		vector[this->index].key = key;
+		this->index++;
+		return vector[this->index - 1].value;
+	}
+	
+	void Set(K k, V v)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			if (vector[i].key == k)
+			{
+				vector[i].value = v;
+				size++;
+				return;
+			}
+		}
+		return;
+	}
+	
+	bool Get(const K& k, V& v)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			if (vector[i].key == k)
+			{
+				v = vector[i].value;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	int Count() const
+	{
+		return index;
+	}
+
+	void Clear()
+	{
+		size = 0;
+		index = 0;
+		delete[] vector;
+	}
+	
 };
+
